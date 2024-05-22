a41bc8643a0434c96e7d1d081e5d50fa4c1b8cd4
Enrique1311 <ejspinelli131176@gmail.com>
2024-05-21 22:38:25 -0300

    Working...

--- main.js
+++ main.js
@@ -23,11 +23,79 @@ for (let i = 1; i <= questionsAmount; i++) {
 function startBtn() {
 	const $startScreen = d.querySelector(".start-screen");
 	const $gameScreen = d.querySelector(".game-screen");
+	const $timer = d.querySelector(".timer");
+
+	const gameTime = 10;
+	// let currentQuestion = -1;
+	// let questionsState = [
+	// 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	// 	0,
+	// ];
+
+	const startGame = () => {
+		let timeLeft = gameTime;
+		let countdown = setInterval(() => {
+			timeLeft--;
+			$timer.innerHTML = timeLeft;
+
+			if (timeLeft === 0) {
+				clearInterval(countdown);
+			}
+		}, 1000);
+	};
+
+	const downloadQuestions = () => {
+		const $currentAnswerLetter = d.querySelector(".current-answer-letter"),
+			$currentQuestion = d.querySelector(".current-question");
+
+		fetch("game_db/questions.json")
+			.then((res) => (res.ok ? res.json() : Promise.reject(res)))
+			.then((res) => {
+				let data = res.questions;
+				data.forEach((el, index) => {
+					if (index <= data.length) {
+						if (el.answered === "false") {
+							$currentAnswerLetter.textContent = el.id;
+							$currentQuestion.textContent = el.question;
+						} else {
+						}
+					}
+				});
+			})
+			.catch((err) => {
+				let message = err.statusText || "Ocurrió un error";
+			});
+
+		// currentQuestion++;
+
+		// if (currentQuestion > questionsAmount) {
+		// 	currentQuestion = 0;
+		// }
+
+		// if (questionsState.indexOf(0) >= 0) {
+		// 	while (questionsState[currentQuestion] == 1) {
+		// 		currentQuestion++;
+		// 		if (currentQuestion >= questionsAmount) {
+		// 			currentQuestion = 0;
+		// 		}
+		// 	}
+
+		// 	$currentAnswerLetter.textContent = data[currentQuestion].id;
+		// 	$currentQuestion.textContent = data[currentQuestion].question;
+		// 	let letter = data[currentQuestion].id;
+		// 	d.getElementById(letter).classList.add("current-question");
+		// } else {
+		// 	clearInterval(countdown);
+		// 	// showResults()
+		// }
+	};
 
 	d.addEventListener("click", (e) => {
 		if (e.target.matches(".start-btn") || e.target.matches(".start-btn i")) {
 			$startScreen.style.display = "none";
 			$gameScreen.style.display = "block";
+			startGame();
+			downloadQuestions();
 		}
 	});
 }
