d06a241a249e76989e5e4206ba55056f7a1ad8e7
Enrique1311 <ejspinelli131176@gmail.com>
2024-05-24 23:09:59 -0300

    Working...

--- main.js
+++ main.js
@@ -1,102 +1,173 @@
 const d = document;
 const questionsAmount = 26;
+let currentQuestion = -1;
 
-const $gameCircle = d.querySelector(".game-circle");
+const questionsState = new Array(questionsAmount).fill(0);
 
-for (let i = 1; i <= questionsAmount; i++) {
-	const $letterContainer = d.createElement("div");
-	$letterContainer.classList.add("letter-container");
-	$letterContainer.textContent = String.fromCharCode(i + 96);
-	$letterContainer.id = String.fromCharCode(i + 96);
-	$gameCircle.appendChild($letterContainer);
+// Download questions ****************************************************************
 
-	const angle = ((i - 1) / questionsAmount) * Math.PI * 2 - Math.PI / 2;
-	const x = Math.round(123 + 140 * Math.cos(angle));
-	const y = Math.round(123 + 140 * Math.sin(angle));
+// function getQuestions() {
+// 	fetch("game_db/questions.json")
+// 		.then((res) => (res.ok ? res.json() : Promise.reject(res)))
+// 		.then((res) => {
+// 			let data = res.questions;
+// 			return data;
+// 		})
+// 		.catch((err) => {
+// 			let message = err.statusText || "Ocurrió un error";
+// 		});
+// 	return data;
+// }
+// getQuestions();
 
-	$letterContainer.style.left = `${x}px`;
-	$letterContainer.style.top = `${y}px`;
+function createRosco() {
+	const $gameCircle = d.querySelector(".game-circle");
+
+	for (let i = 1; i <= questionsAmount; i++) {
+		const $letterContainer = d.createElement("div");
+		$letterContainer.classList.add("rosco-letter-container");
+		$letterContainer.textContent = String.fromCharCode(i + 96);
+		$letterContainer.id = String.fromCharCode(i + 96).toUpperCase;
+		$gameCircle.appendChild($letterContainer);
+
+		const angle = ((i - 1) / questionsAmount) * Math.PI * 2 - Math.PI / 2;
+		const x = Math.round(123 + 140 * Math.cos(angle));
+		const y = Math.round(123 + 140 * Math.sin(angle));
+
+		$letterContainer.style.left = `${x}px`;
+		$letterContainer.style.top = `${y}px`;
+	}
 }
 
 // Start Screen ************************************************
 
-function startBtn() {
+function startBtn(data) {
 	const $startScreen = d.querySelector(".start-screen");
 	const $gameScreen = d.querySelector(".game-screen");
-	const $timer = d.querySelector(".timer");
-
-	const gameTime = 10;
-	// let currentQuestion = -1;
-	// let questionsState = [
-	// 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	// 	0,
-	// ];
-
-	const startGame = () => {
-		let timeLeft = gameTime;
-		let countdown = setInterval(() => {
-			timeLeft--;
-			$timer.innerHTML = timeLeft;
-
-			if (timeLeft === 0) {
-				clearInterval(countdown);
-			}
-		}, 1000);
-	};
-
-	const downloadQuestions = () => {
-		const $currentAnswerLetter = d.querySelector(".current-answer-letter"),
-			$currentQuestion = d.querySelector(".current-question");
-
-		fetch("game_db/questions.json")
-			.then((res) => (res.ok ? res.json() : Promise.reject(res)))
-			.then((res) => {
-				let data = res.questions;
-				data.forEach((el, index) => {
-					if (index <= data.length) {
-						if (el.answered === "false") {
-							$currentAnswerLetter.textContent = el.id;
-							$currentQuestion.textContent = el.question;
-						} else {
-						}
-					}
-				});
-			})
-			.catch((err) => {
-				let message = err.statusText || "Ocurrió un error";
-			});
-
-		// currentQuestion++;
-
-		// if (currentQuestion > questionsAmount) {
-		// 	currentQuestion = 0;
-		// }
-
-		// if (questionsState.indexOf(0) >= 0) {
-		// 	while (questionsState[currentQuestion] == 1) {
-		// 		currentQuestion++;
-		// 		if (currentQuestion >= questionsAmount) {
-		// 			currentQuestion = 0;
-		// 		}
-		// 	}
-
-		// 	$currentAnswerLetter.textContent = data[currentQuestion].id;
-		// 	$currentQuestion.textContent = data[currentQuestion].question;
-		// 	let letter = data[currentQuestion].id;
-		// 	d.getElementById(letter).classList.add("current-question");
-		// } else {
-		// 	clearInterval(countdown);
-		// 	// showResults()
-		// }
-	};
 
 	d.addEventListener("click", (e) => {
 		if (e.target.matches(".start-btn") || e.target.matches(".start-btn i")) {
 			$startScreen.style.display = "none";
 			$gameScreen.style.display = "block";
-			startGame();
-			downloadQuestions();
+			createRosco();
+			writeQuestions();
+			startTimer();
 		}
 	});
 }
 startBtn();
+
+// Game timer ************************************************
+
+function startTimer() {
+	const gameTime = 3;
+	const $timer = d.querySelector(".timer");
+
+	let timeLeft = gameTime;
+	let countdown = setInterval(() => {
+		timeLeft--;
+		$timer.innerHTML = timeLeft;
+
+		if (timeLeft === 0) {
+			clearInterval(countdown);
+			setTimeout(() => {
+				alert("Game over!");
+			}, 1000);
+		}
+	}, 1000);
+}
+
+// Write questions ****************************************************************
+
+function writeQuestions() {
+	const $currentAnswerLetter = d.querySelector(".current-answer-letter"),
+		$currentQuestion = d.querySelector(".current-question"),
+		$roscoLetterContainer = d.querySelector(".rosco-letter-container");
+
+	fetch("game_db/questions.json")
+		.then((res) => (res.ok ? res.json() : Promise.reject(res)))
+		.then((res) => {
+			let data = res.questions;
+
+			currentQuestion++;
+
+			if (currentQuestion >= questionsAmount) {
+				currentQuestion = 0;
+			}
+
+			if (questionsState.indexOf(0) >= 0) {
+				while (questionsState[currentQuestion] === 1) {
+					currentQuestion++;
+					if (currentQuestion >= questionsAmount) {
+						currentQuestion = 0;
+					}
+				}
+				$currentAnswerLetter.textContent = data[currentQuestion].id;
+				$currentQuestion.textContent = data[currentQuestion].question;
+
+				let letter = data[currentQuestion].id;
+				$roscoLetterContainer.classList.add("active-letter");
+			} else {
+				clearInterval(countdown);
+			}
+		})
+		.catch((err) => {
+			let message = err.statusText || "Ocurrió un error";
+		});
+}
+
+// Update questions ****************************************************************
+
+function getQuestions() {
+	fetch("game_db/questions.json")
+		.then((res) => (res.ok ? res.json() : Promise.reject(res)))
+		.then((res) => {
+			let data = res.questions;
+			return data;
+		})
+		.catch((err) => {
+			let message = err.statusText || "Ocurrió un error";
+		});
+}
+
+// function getQuestions() {
+// 	const $currentAnswerLetter = d.querySelector(".current-answer-letter"),
+// 		$currentQuestion = d.querySelector(".current-question");
+
+// 	fetch("game_db/questions.json")
+// 		.then((res) => (res.ok ? res.json() : Promise.reject(res)))
+// 		.then((res) => {
+// 			let data = res.questions;
+// 			data.forEach((el, index) => {
+// 				if (index <= data.length && el.answered === "false") {
+// 					$currentAnswerLetter.textContent = el.id;
+// 					$currentQuestion.textContent = el.question;
+// 					getAnswer();
+// 				}
+// 			});
+// 		})
+// 		.catch((err) => {
+// 			let message = err.statusText || "Ocurrió un error";
+// 		});
+// }
+
+// function getAnswer(el) {
+// 	let $answer = d.querySelector(".answer");
+// 	$answer.addEventListener("keyup", (e) => {
+// 		if (e.keyCode === 13) {
+// 			if ($answer.value == "") {
+// 				alert("Please enter an answer!");
+// 				return;
+// 			}
+// 			let answerText = $answer.value.toUpperCase();
+// 			console.log(el);
+// 			if (answerText === el.answer.toUpperCase()) {
+// 				alert("Correct answer!");
+// 				return;
+// 			} else {
+// 				alert("Wrong answer!");
+// 				return;
+// 			}
+// 		}
+// 	});
+// }
