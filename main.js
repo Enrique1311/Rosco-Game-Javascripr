7e9dbff71cb288033fa67e6223369256d12e073a
Enrique1311 <ejspinelli131176@gmail.com>
2024-05-25 17:23:18 -0300

    Working...

--- main.js
+++ main.js
@@ -1,33 +1,21 @@
-const d = document;
+const d = document,
+	$gameCircle = d.querySelector(".game-circle"),
+	$startScreen = d.querySelector(".start-screen"),
+	$gameScreen = d.querySelector(".game-screen"),
+	$timer = d.querySelector(".timer");
+
 const questionsAmount = 26;
-let currentQuestion = -1;
+let currentQuestion = -1,
+	correctsAnswers = 0;
 
 const questionsState = new Array(questionsAmount).fill(0);
 
-// Download questions ****************************************************************
-
-// function getQuestions() {
-// 	fetch("game_db/questions.json")
-// 		.then((res) => (res.ok ? res.json() : Promise.reject(res)))
-// 		.then((res) => {
-// 			let data = res.questions;
-// 			return data;
-// 		})
-// 		.catch((err) => {
-// 			let message = err.statusText || "Ocurrió un error";
-// 		});
-// 	return data;
-// }
-// getQuestions();
-
 function createRosco() {
-	const $gameCircle = d.querySelector(".game-circle");
-
 	for (let i = 1; i <= questionsAmount; i++) {
 		const $letterContainer = d.createElement("div");
 		$letterContainer.classList.add("rosco-letter-container");
 		$letterContainer.textContent = String.fromCharCode(i + 96);
-		$letterContainer.id = String.fromCharCode(i + 96).toUpperCase;
+		$letterContainer.id = String.fromCharCode(i + 96);
 		$gameCircle.appendChild($letterContainer);
 
 		const angle = ((i - 1) / questionsAmount) * Math.PI * 2 - Math.PI / 2;
@@ -41,10 +29,7 @@ function createRosco() {
 
 // Start Screen ************************************************
 
-function startBtn(data) {
-	const $startScreen = d.querySelector(".start-screen");
-	const $gameScreen = d.querySelector(".game-screen");
-
+function startBtn() {
 	d.addEventListener("click", (e) => {
 		if (e.target.matches(".start-btn") || e.target.matches(".start-btn i")) {
 			$startScreen.style.display = "none";
@@ -60,8 +45,7 @@ startBtn();
 // Game timer ************************************************
 
 function startTimer() {
-	const gameTime = 3;
-	const $timer = d.querySelector(".timer");
+	const gameTime = 20;
 
 	let timeLeft = gameTime;
 	let countdown = setInterval(() => {
@@ -81,8 +65,7 @@ function startTimer() {
 
 function writeQuestions() {
 	const $currentAnswerLetter = d.querySelector(".current-answer-letter"),
-		$currentQuestion = d.querySelector(".current-question"),
-		$roscoLetterContainer = d.querySelector(".rosco-letter-container");
+		$currentQuestion = d.querySelector(".current-question");
 
 	fetch("game_db/questions.json")
 		.then((res) => (res.ok ? res.json() : Promise.reject(res)))
@@ -104,9 +87,11 @@ function writeQuestions() {
 				}
 				$currentAnswerLetter.textContent = data[currentQuestion].id;
 				$currentQuestion.textContent = data[currentQuestion].question;
+				let currentAnswer = data[currentQuestion].answer;
 
-				let letter = data[currentQuestion].id;
-				$roscoLetterContainer.classList.add("active-letter");
+				let letter = data[currentQuestion].id.toLowerCase();
+				d.getElementById(letter).classList.add("active-letter");
+				controlAnswer(currentAnswer, data);
 			} else {
 				clearInterval(countdown);
 			}
@@ -130,44 +115,29 @@ function getQuestions() {
 		});
 }
 
-// function getQuestions() {
-// 	const $currentAnswerLetter = d.querySelector(".current-answer-letter"),
-// 		$currentQuestion = d.querySelector(".current-question");
+function controlAnswer(currentAnswer, data) {
+	let $answer = d.querySelector(".answer");
 
-// 	fetch("game_db/questions.json")
-// 		.then((res) => (res.ok ? res.json() : Promise.reject(res)))
-// 		.then((res) => {
-// 			let data = res.questions;
-// 			data.forEach((el, index) => {
-// 				if (index <= data.length && el.answered === "false") {
-// 					$currentAnswerLetter.textContent = el.id;
-// 					$currentQuestion.textContent = el.question;
-// 					getAnswer();
-// 				}
-// 			});
-// 		})
-// 		.catch((err) => {
-// 			let message = err.statusText || "Ocurrió un error";
-// 		});
-// }
+	$answer.addEventListener("keyup", (e) => {
+		if (e.keyCode === 13) {
+			if ($answer.value == "") {
+				alert("Please enter an answer!");
+				return;
+			} else {
+				let answerText = $answer.value.toUpperCase();
+				questionsState[currentQuestion] = 1;
+				let letter = data[currentQuestion].id.toLowerCase();
+				d.getElementById(letter).classList.remove("active-letter");
 
-// function getAnswer(el) {
-// 	let $answer = d.querySelector(".answer");
-// 	$answer.addEventListener("keyup", (e) => {
-// 		if (e.keyCode === 13) {
-// 			if ($answer.value == "") {
-// 				alert("Please enter an answer!");
-// 				return;
-// 			}
-// 			let answerText = $answer.value.toUpperCase();
-// 			console.log(el);
-// 			if (answerText === el.answer.toUpperCase()) {
-// 				alert("Correct answer!");
-// 				return;
-// 			} else {
-// 				alert("Wrong answer!");
-// 				return;
-// 			}
-// 		}
-// 	});
-// }
+				if (answerText === currentAnswer.toUpperCase()) {
+					d.getElementById(letter).classList.add("correct-answered");
+				} else {
+					$answer.value = "";
+					d.getElementById(letter).classList.add("wrong-answered");
+				}
+				$answer.value = "";
+				writeQuestions();
+			}
+		}
+	});
+}
