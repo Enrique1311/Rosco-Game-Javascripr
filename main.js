26b22d63fae97280bc51504d29eb48b5c6e8ebdb
Enrique1311 <ejspinelli131176@gmail.com>
2024-05-27 17:54:07 -0300

    Working...

--- main.js
+++ main.js
@@ -2,13 +2,20 @@ const d = document,
 	$gameCircle = d.querySelector(".game-circle"),
 	$startScreen = d.querySelector(".start-screen"),
 	$gameScreen = d.querySelector(".game-screen"),
+	$resultsScreen = d.querySelector(".results-screen"),
 	$timer = d.querySelector(".timer"),
-	$passBtn = d.querySelector(".pass-btn");
+	$passBtn = d.querySelector(".pass-btn"),
+	$correctAnsResult = d.querySelector(".correct-ans-result span"),
+	$wrongAnsResult = d.querySelector(".wrong-ans-result span"),
+	$scoreResult = d.querySelector(".score-result span");
+let $answer = d.querySelector(".answer");
 
 const questionsAmount = 26;
+const gameTime = 10;
 let data = [];
 let currentQuestion = -1,
-	correctsAnswers = 0,
+	correctAnswers = 0,
+	wrongAnswers = 0,
 	currentAnswer = "";
 
 const questionsState = new Array(questionsAmount).fill(0);
@@ -35,6 +42,7 @@ function createRosco() {
 		$letterContainer.textContent = String.fromCharCode(i + 96);
 		$letterContainer.id = String.fromCharCode(i + 96).toUpperCase();
 		$gameCircle.appendChild($letterContainer);
+		$timer.textContent = gameTime;
 
 		const angle = ((i - 1) / questionsAmount) * Math.PI * 2 - Math.PI / 2;
 		const x = Math.round(123 + 140 * Math.cos(angle));
@@ -50,21 +58,24 @@ function createRosco() {
 function startBtn() {
 	d.addEventListener("click", (e) => {
 		if (e.target.matches(".start-btn") || e.target.matches(".start-btn i")) {
-			$startScreen.style.display = "none";
-			$gameScreen.style.display = "block";
 			createRosco();
-			writeQuestions();
-			startTimer();
+			startGame();
 		}
 	});
 }
 startBtn();
 
+function startGame() {
+	$startScreen.style.display = "none";
+	$resultsScreen.style.display = "none";
+	$gameScreen.style.display = "block";
+	writeQuestions();
+	startTimer();
+}
+
 // Game timer ************************************************
 
 function startTimer() {
-	const gameTime = 20;
-
 	let timeLeft = gameTime;
 	let countdown = setInterval(() => {
 		timeLeft--;
@@ -73,7 +84,7 @@ function startTimer() {
 		if (timeLeft === 0) {
 			clearInterval(countdown);
 			setTimeout(() => {
-				alert("Game over!");
+				showFinalScreen();
 			}, 1000);
 		}
 	}, 1000);
@@ -105,12 +116,11 @@ function writeQuestions() {
 		d.getElementById(letter).classList.add("active-letter");
 	} else {
 		clearInterval(countdown);
+		showFinalScreen();
 	}
 }
 
 function getAnswer() {
-	let $answer = d.querySelector(".answer");
-
 	$answer.addEventListener("keyup", (e) => {
 		if (e.keyCode === 13) {
 			if ($answer.value == "") {
@@ -127,13 +137,14 @@ getAnswer();
 
 function controlAnswer(answerText, $answer) {
 	if (answerText === data[currentQuestion].answer.toUpperCase()) {
-		correctsAnswers++;
+		correctAnswers++;
 		questionsState[currentQuestion] = 1;
 		let letter = data[currentQuestion].id;
 		d.getElementById(letter).classList.remove("active-letter");
 		d.getElementById(letter).classList.add("correct-answered");
 	} else {
 		questionsState[currentQuestion] = 1;
+		wrongAnswers++;
 		let letter = data[currentQuestion].id;
 		d.getElementById(letter).classList.remove("active-letter");
 		d.getElementById(letter).classList.add("wrong-answered");
@@ -147,5 +158,41 @@ d.addEventListener("click", (e) => {
 		let letter = data[currentQuestion].id;
 		d.getElementById(letter).classList.remove("active-letter");
 		writeQuestions();
+	}
+});
+
+// Final Results Screen ****************************************************************
+
+function showFinalScreen() {
+	$correctAnsResult.textContent = correctAnswers;
+	$wrongAnsResult.textContent = wrongAnswers;
+	$scoreResult.textContent = Math.round(
+		(correctAnswers * 100) / questionsAmount
+	);
+
+	$gameScreen.style.display = "none";
+	$resultsScreen.style.display = "block";
+}
+
+// Try Again Button ********************************
+
+d.addEventListener("click", (e) => {
+	if (e.target.matches(".try-again-btn")) {
+		$roscoLetterContainers = d.querySelectorAll(".rosco-letter-container");
+		currentQuestion = -1;
+		timeLeft = 0;
+		$timer.innerText = gameTime;
+		correctAnswers = 0;
+		wrongAnswers = 0;
+		currentAnswer = "";
+		questionsState.fill(0);
+		$answer.value = "";
+		console.log($roscoLetterContainers);
+		for (i = 0; i < $roscoLetterContainers.length; i++) {
+			$roscoLetterContainers[i].classList.remove("correct-answered");
+			$roscoLetterContainers[i].classList.remove("wrong-answered");
+			$roscoLetterContainers[i].classList.remove("active-letter");
+		}
+		startGame();
 	}
 });
